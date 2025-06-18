64a9bb5631aeee17ae53084f66f06cef87cd3a30
Felipezxc <felipe.miranda377@gmail.com>
2025-06-18 19:29:45 -0300

    divisão

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-const divisão = require('./divisão');
-console.log(divisão(10, 2));
+// index.js
+const divisao = require('./divisao');
+
+// Exemplos de divisão com positivos e negativos
+const exemplos = [
+  { a: 10, b: 5 },
+  { a: -8, b: 2 },
+  { a: 4, b: -2 },
+  { a: -10, b: -5 },
+  { a: 0, b: 5 },
+  { a: 5, b: 0 } // esse caso mostra erro
+];
+
+exemplos.forEach(({ a, b }) => {
+  const resultado = divisao(a, b);
+  console.log(`Divisão: ${a} / ${b} = ${resultado}`);
+});
